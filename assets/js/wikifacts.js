65443cf83ca5ace7b07af474f525aa94cdd27304
McKenna Larson <mklarson14@gmail.com>
2021-07-09 08:14:21 -0700

    renamed files

---
+++ assets/js/wikifacts.js
@@ -0,0 +1,4 @@
+// test
+console.log("wikifacts.js")
+
+//https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=&srlimit=20&srsearch=SEARCH_QUERY_GOES_HERE
